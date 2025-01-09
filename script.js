0487d029deac421b2e9e7d5937dee9b98034ccb7
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-01-09 14:50:54 +0530

    API is getting fetched

---
+++ script.js
@@ -0,0 +1,26 @@
+// api url
+const BASE_URL = "https://api.weatherapi.com/v1/current.json?key=43bdaf4efe3949e0bc5151325250801"
+
+// elements we need
+const input = document.querySelector(".city-name")
+const conditionImg = document.querySelector("#condition-img")
+const temp = document.querySelector(".temp")
+const city = document.querySelector(".city")
+const humid = document.querySelector(".humid-percent")
+const wind = document.querySelector(".wind-detail")
+
+// logical variables
+
+// functions that are needed
+async function getWeather(name){
+    const URL = `${BASE_URL}&q=${name.toUpperCase()}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+}
+// event listeners
+input.addEventListener("keydown",(event) => {
+    if(event.key === "Enter"){
+        getWeather(input.value);
+    }
+});
